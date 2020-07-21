21a600a00a249281bbbbe033fcc55d662ea46202
Ridleh <42791844+Ridleh@users.noreply.github.com>
2020-07-20 22:05:16 -0500

    switch to tab navigator

--- Navigation/NavigationIndex.tsx
+++ Navigation/NavigationIndex.tsx
@@ -9,42 +9,59 @@ import RecruitScreen from "../Screens/RecruitScreen";
 import CharacterScreen from "../Screens/CharacterScreen";
 import CharacterStack from "./CharacterStack";
 
+import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import QuestStack from "./QuestStack";
+import { getImageFromIconsMap } from "../AssetMaps/IconsMap";
 
-const Drawer = createDrawerNavigator();
+const Drawer = createBottomTabNavigator();
 
 export function Navigation() {
   return (
     <NavigationContainer>
       <Drawer.Navigator
         initialRouteName="Home"
-        drawerStyle={{
-          backgroundColor: "#4A3531",
-        }}
+        tabBarOptions={{activeBackgroundColor:'orange'}}
       >
         <Drawer.Screen
           name="Home"
           component={HomeScreen}
-          options={{ unmountOnBlur: true }}
+          options={{ unmountOnBlur: true,
+            tabBarIcon: ({focused}) => TabBarIcon(focused, 'Quest.png')
+           }}
         />
         <Drawer.Screen
           name="Character"
           component={CharacterStack}
-          options={{ unmountOnBlur: true }}
+          options={{ unmountOnBlur: true,
+            tabBarIcon: ({focused}) => TabBarIcon(focused, 'Equip.png') }}
         />
         <Drawer.Screen
         name='Shop'
         component={ShopScreen}
-        options={{unmountOnBlur: true}}
+        options={{unmountOnBlur: true,
+          tabBarIcon: ({focused}) => TabBarIcon(focused, 'greed.png')}}
         />
         <Drawer.Screen
         name='Quests'
         component={QuestStack}
-        options={{unmountOnBlur: true}}
+        options={{unmountOnBlur: true,
+          tabBarIcon: ({focused}) => TabBarIcon(focused, 'Quest.png')}}
         />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const TabBarIcon = (focused: boolean, image: string) => {
+  return (
+    <Image
+      style={{
+        width: focused ? 36 : 30,
+        height: focused ? 36 : 30,
+      }}
+      source={getImageFromIconsMap(image)}
+    />
+  );
+};
