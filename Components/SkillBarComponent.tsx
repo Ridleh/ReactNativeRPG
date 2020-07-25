ed13880f72940ae7982e3e6ea20493b2b2554aed
Ridleh <42791844+Ridleh@users.noreply.github.com>
2020-07-25 15:43:39 -0500

    implement first 2 abilities

--- Components/SkillBarComponent.tsx
+++ Components/SkillBarComponent.tsx
@@ -6,6 +6,8 @@ import { getImageFromUIMap } from "../AssetMaps/UIMap";
 import ItemContainer from "./ItemContainer";
 import { getImageFromFrameBackgroundsMap } from "../AssetMaps/FrameBackgroundsMap";
 import { TouchableHighlight } from "react-native-gesture-handler";
+import { getImageFromIconsFreeMap } from "../AssetMaps/IconsFreeMap";
+
 export default function (props: any) {
   return (
     <View style={styles.flexFullRow}>
@@ -15,24 +17,24 @@ export default function (props: any) {
         resizeMode="stretch"
       >
         <View style={{flex: 1, flexDirection:'row', justifyContent:'space-evenly'}}>
-          <TouchableHighlight style={{ flex: 1 }} onPress={props.handlePress}>
+          <TouchableHighlight style={{ flex: 1 }} onPress={() => props.handlePress(1)}>
             <ItemContainer
-              image={getImageFromFrameBackgroundsMap("helm_background.png")}
+              image={getImageFromIconsFreeMap("skill_icon_01.png")}
             />
           </TouchableHighlight>
-          <TouchableHighlight style={{ flex: 1 }} onPress={props.handlePress}>
+          <TouchableHighlight style={{ flex: 1 }} onPress={() => props.handlePress(2)}>
             <ItemContainer
-              image={getImageFromFrameBackgroundsMap("helm_background.png")}
+              image={getImageFromIconsFreeMap("skill_icon_02.png")}
             />
           </TouchableHighlight>
-          <TouchableHighlight style={{ flex: 1 }} onPress={props.handlePress}>
+          <TouchableHighlight style={{ flex: 1 }} onPress={() => props.handlePress(3)}>
             <ItemContainer
-              image={getImageFromFrameBackgroundsMap("helm_background.png")}
+              image={getImageFromIconsFreeMap("skill_icon_03.png")}
             />
           </TouchableHighlight>
-          <TouchableHighlight style={{ flex: 1 }} onPress={props.handlePress}>
+          <TouchableHighlight style={{ flex: 1 }} onPress={() => props.handlePress(4)}>
             <ItemContainer
-              image={getImageFromFrameBackgroundsMap("helm_background.png")}
+              image={getImageFromIconsFreeMap("skill_icon_04.png")}
             />
           </TouchableHighlight>
         </View>
--- Screens/BattleScreen.tsx
+++ Screens/BattleScreen.tsx
@@ -19,8 +19,12 @@ export default class HomeScreen extends Component<any, any> {
         {
           id: 1,
           name: "Player",
-          health: 100,
+          health: 7150,
           mana: 75,
+          attack: 227,
+          defense: 170,
+          magic: 300,
+          resistance: 141,
           image: getImageFromCharactersMap("tyro.png"),
         },
       ],
@@ -28,7 +32,11 @@ export default class HomeScreen extends Component<any, any> {
         {
           id: 2,
           name: "enemy",
-          health: 100,
+          health: 21712,
+          attack: 488,
+          defense: 600,
+          magic: 496,
+          resistance: 600,
           mana: 75,
           image: getImageFromCharactersMap("tyro.png"),
         },
@@ -37,12 +45,86 @@ export default class HomeScreen extends Component<any, any> {
     };
   }
 
-  openDrawer = () => {
-    this.props.navigation.openDrawer();
-  };
+  handleAbilityCast = (skillNum: number) => {
+    console.log("player's attack", skillNum);
+    const ability: any = this.getAbility(skillNum);
+    var damage: number;
+    var player: any = this.state.playableCharacters[0];
+    var enemy: any = this.state.opponents[0];
+    if (ability.damageType === "physical") {
+      console.log("physical attack");
+      const attack: number = player.attack * ability.ratio;
+      damage = Math.pow(attack, 1.8) / Math.pow(enemy.defense, 0.5);
+      damage = Math.round(damage);
+      console.log("damage is", damage);
+    } else {
+      console.log("magical attack");
+      const attack: number = player.magic * ability.ratio;
+      damage = Math.pow(attack, 1.65) / Math.pow(enemy.defense, 0.5);
+      damage = Math.round(damage);
+      console.log("damage is", damage);
+    }
+    //const test :string[] = [];
+    //test.length
+    if (ability.bonusActions.length >= 1) {
+      for (var i = 0; i < ability.bonusActions.length; i++) {
+        const action: any = ability.bonusActions[i];
+        var actionDamage: number;
+        if (action.damageSource === "old") {
+          actionDamage = damage * action.ratio;
+        } else {
+          console.log("wrong branch!!!");
+          actionDamage = 100;
+        }
+        if (action.target === "self") {
+          if (action.damageORHeal === "heal") {
+            player.health += actionDamage;
+          } else {
+            player.health -= actionDamage;
+          }
+        } else {
+          console.log('wrong target branch!!!');
+          //'add this branch later'
+        }
 
-  handleAbilityCast = () => {
-    console.log("player's attack");
+        /*
+        target: self,
+            damageORHeal: 'heal',
+            ratio: 1,
+            buffs:[],
+            debuffs: []
+        */
+      }
+    }
+    var newPlayers: any[] = [];
+    newPlayers.push(player);
+
+    enemy.health -= damage;
+    var newOpponents: any[] = [];
+    newOpponents.push(enemy);
+    this.setState({ opponents: newOpponents, playableCharacters: newPlayers });
+    if (enemy.health <= 0) {
+      console.log("enemy is defeated. game over");
+      this.props.navigation.pop();
+    } else {
+      setTimeout(() => {
+        console.log("enemy's attack");
+        const enemyAttack: number = enemy.attack * 1.5;
+        var enemyDamage = Math.pow(enemyAttack, 2) / Math.pow(player.defense, 0.84);
+        enemyDamage = Math.round(damage);
+        console.log("damage is", enemyDamage);
+        player.health -= enemyDamage;
+        var newPlayers: any[] = [];
+        newPlayers.push(player);
+        this.setState({ playableCharacters: newPlayers });
+        if (player.health <= 0) {
+          console.log("player is defeated. game over");
+          this.props.navigation.pop();
+        }
+      }, 2000);
+    }
+
+    /*
     const damage: number = Math.floor(Math.random() * 80);
     var enemy: any = this.state.opponents[0];
     enemy.health -= damage;
@@ -67,6 +149,60 @@ export default class HomeScreen extends Component<any, any> {
         }
       }, 2000);
     }
+    */
+  };
+
+  getAbility = (skillNum: number) => {
+    if (skillNum === 1) {
+      return {
+        target: "enemy",
+        damageType: "magical",
+        ratio: 0.6,
+        numHits: 1,
+        damageORHeal: "damage",
+        canCrit: false,
+        bonusActions: [
+          {
+            target: 'self',
+            damageORHeal: "heal",
+            damageSource: "old",
+            ratio: 1,
+            buffs: [],
+            debuffs: [],
+          },
+        ],
+      };
+    } else if (skillNum === 2) {
+      return {
+        target: "enemy",
+        damageType: "physical",
+        ratio: 1.5,
+        numHits: 1,
+        damageORHeal: "damage",
+        canCrit: true,
+        bonusActions: []
+      };
+    } else if (skillNum === 3) {
+      console.log("dont use this yet");
+      return {
+        target: "enemy",
+        damageType: "physical",
+        ratio: 1.5,
+        numHits: 1,
+        damageORHeal: "damage",
+        canCrit: true,
+      };
+    } else {
+      console.log("dont use this yet");
+      return {
+        target: "enemy",
+        damageType: "physical",
+        ratio: 1.5,
+        numHits: 1,
+        damageORHeal: "damage",
+        canCrit: true,
+      };
+    }
   };
 
   renderPlayer = (player: any) => {
@@ -74,7 +210,7 @@ export default class HomeScreen extends Component<any, any> {
       <View>
         <HealthBar
           currentHealth={player.item.health}
-          totalHealth={100}
+          totalHealth={player.item.health}
           label={player.item.name}
         />
         <Image
@@ -94,7 +230,7 @@ export default class HomeScreen extends Component<any, any> {
       <View>
         <HealthBar
           currentHealth={enemy.item.health}
-          totalHealth={100}
+          totalHealth={enemy.item.health}
           label={enemy.item.name}
         />
         <Image
@@ -141,7 +277,7 @@ export default class HomeScreen extends Component<any, any> {
                 >
                   <FlatList
                     data={this.state.opponents}
-                    renderItem={(player) => this.renderPlayer(player)}
+                    renderItem={(player) => this.renderEnemy(player)}
                     keyExtractor={(player: any) => player.id.toString()}
                   />
                 </View>
